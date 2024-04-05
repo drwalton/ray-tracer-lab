517b18c288c6bb8e772352a720a89b07d84edf93
David R. Walton <drwalton@users.noreply.github.com>
2024-04-05 12:36:13 +0100

    Add comments

--- HitInfo.hpp
+++ HitInfo.hpp
@@ -3,10 +3,16 @@
 
 class Shader;
 
+/// <summary>
+/// Structure encoding information from an intersection test.
+/// </summary>
 struct HitInfo
 {
-	float hitT;
-	Eigen::Vector3f normal, location, inDirection;
-	const Shader* shader;
-	Eigen::Vector2f texCoords;
+	float hitT; // Distance along the ray the hit occurred at.
+	Eigen::Vector3f 
+		normal, // Normal vector of the object at the hit location.
+		location, // World-space location of hit point.
+		inDirection; // Incoming ray direction.
+	Eigen::Vector2f texCoords; // Texture coordinates at the hit location.
+	const Shader* shader; // Shader associated with the hit object.
 };
